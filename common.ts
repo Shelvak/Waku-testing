8394d1f2f82e6417986744c0888dd0d1d26f5e5a
Néstor Coppi <nestorcoppi@gmail.com>
2025-01-01 20:26:00 -0300

    ASd

--- common.ts
+++ common.ts
@@ -15,40 +15,85 @@ export const HANDSHAKE_TOPIC = CONTENT_TOPIC.replace('PLACEHOLDER', 'handshake')
 
 export const sleep = async (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
+const to_bs58 = (pubKey) => {
+  if (!pubKey) return 'unknown';
+
+  if (pubKey.length === 32) {
+    return bytesToUtf8(pubKey);
+  }
+}
+
+
 // Protobuf bytes => Uint8Array (browser) or Buffer (node)
 // repeated indicates an array of bytes
 export const ChatMessage = new protobuf.Type("ChatMessage")
   .add(new protobuf.Field("timestamp", 1, "uint64"))
   .add(new protobuf.Field("state", 2, "string"))
-  .add(new protobuf.Field("text", 3, "bytes", "repeated"))
-  .add(new protobuf.Field("replyTo", 4, "bytes"));
+  .add(new protobuf.Field("text", 3, "bytes"))
+  .add(new protobuf.Field("replyTo", 4, "bytes"))
+  .add(new protobuf.Field("pubKey", 5, "bytes"))
+  .add(new protobuf.Field("signedMsg", 6, "bytes"));
 
-export const sendMsg = async (node, topic, replyTo, state, ...msg) => {
-  console.log("sendmsg", {
+export const sendMsg = async ({
+  node,
+  topic,
+  replyTo,
+  state,
+  pubKey,
+  signedMsg,
+  msg,
+}: {
+  node: any,
+  topic: string,
+  replyTo: string,
+  state: string,
+  pubKey?: string,
+  signedMsg?: string,
+  msg: object
+}) => {
+  console.log("sendMsg", {
     topic,
     timestamp: Date.now(),
-    text: msg.flat(Infinity).map(utf8ToBytes),
     state,
+    text: msg,
     replyTo,
+    pubKey,
+    signedMsg,
   })
+  try {
   const protoMessage = ChatMessage.create({
     timestamp: Date.now(),
-    text: msg.flat(Infinity).map(utf8ToBytes),
-    state,
+    text: utf8ToBytes(JSON.stringify(msg)),
     replyTo: utf8ToBytes(replyTo),
+    state,
+    pubKey: pubKey ? utf8ToBytes(pubKey) : undefined,
+    signedMsg: signedMsg ? utf8ToBytes(signedMsg) : undefined,
   });
 
   await node.lightPush.send(
-    createEncoder({contentTopic: topic}),
+    createEncoder({contentTopic: topic}), // ephemeral: true to not store
     { payload: ChatMessage.encode(protoMessage).finish() }
   );
 
   console.log("Message sent!");
+  } catch (e) {
+    console.error('Error sending message:', e);
+  }
 }
 
+
 export const subscribeTo = async (node, topic, fn) => {
+  let error, subscription
   // @ts-ignore
-  const { error, subscription } = await node.filter.createSubscription({ contentTopics: [topic] });
+  try {
+    ({ error, subscription } = await node.filter.createSubscription({
+      forceUseAllPeers: true,
+      maxAttempts: 10,
+      contentTopics: [topic] }));
+  } catch (e) {
+    console.error('Error creating subscription:', e);
+    process.exit(1);
+  }
 
   if (error) {
     console.error("Error creating subscription", error);
@@ -58,17 +103,22 @@ export const subscribeTo = async (node, topic, fn) => {
   await subscription.subscribe(
     [createDecoder(topic)],
     async (wakuMessage) => {
-      const msg = ChatMessage.decode(wakuMessage.payload);
+      try {
+        const msg = ChatMessage.decode(wakuMessage.payload);
 
-      msg.text = msg.text.map(bytesToUtf8);
-      if (msg.replyTo)
-        msg.replyTo = bytesToUtf8(msg.replyTo);
+        msg.text = JSON.parse(bytesToUtf8(msg.text))
 
-      console.log(`\n\n\n [${msg.state}] ${
-        (new Date(parseInt(msg.timestamp))).toLocaleString()
-      } # ${msg.text}\n\n\n`);
+        if (msg.replyTo)
+          msg.replyTo = bytesToUtf8(msg.replyTo);
 
-      await fn(node, topic, msg);
+        console.log(`\n\n\n [${msg.state}] ${
+          (new Date(parseInt(msg.timestamp))).toLocaleString()
+        } [${to_bs58(msg.pubKey)}] # ${msg.text}\n\n\n`);
+
+        await fn(node, topic, msg);
+      } catch (e) {
+        console.error('Error decoding message:', e);
+      }
     }
   );
 
@@ -78,8 +128,12 @@ export const subscribeTo = async (node, topic, fn) => {
       await subscription.ping();
       break;
     } catch (e) {
+      if (e instanceof Error && e.message.includes("peer has no subscriptions")) {
+                  // Reinitiate the subscription if the ping fails
+        return await subscribeTo(node, topic, fn);
+      }
       console.log("Error pinging subscription " + i)
-      await sleep(500)
+      await sleep(1000)
     }
   }
 
@@ -87,18 +141,22 @@ export const subscribeTo = async (node, topic, fn) => {
 }
 
 export const startNode = async () => {
-  const node = await createLightNode({ defaultBootstrap: true });
+  const node = await createLightNode({
+    defaultBootstrap: true
+  });
 
   await node.start();
 
   for ( let i = 0; i < 20; i++ )  {
     try {
-      await waitForRemotePeer(node, [Protocols.LightPush, Protocols.Filter], 2000);
+      // last version
+      // await node.waitForPeers([Protocols.LightPush, Protocols.Filter], 5000);
+      await waitForRemotePeer(node, [Protocols.LightPush, Protocols.Filter], 5000);
       if (node.isConnected())
         break
     } catch (e) {
-      console.log("Error waiting for remote peer " + i)
-      await sleep(500)
+      console.log("Error waiting for remote peer " + i, e)
+      await sleep(1000)
     }
   }
 
--- receiver.ts
+++ receiver.ts
@@ -6,19 +6,57 @@ import {
   startNode,
   subscribeTo,
 } from './common'
+import { Keypair } from '@solana/web3.js';
+import nacl from 'tweetnacl';
+import bs58 from 'bs58';
 
 const main = async () => {
   const node = await startNode();
+
+  const keypair = Keypair.fromSecretKey( Uint8Array.from([
+        8, 254, 227,   1,  69,  56, 110, 231, 145,  93, 168,
+        131,  96, 182, 118, 246, 208, 128,  65, 136, 234, 144,
+        199,  34,  53,  15,  96, 144,  50,  82,  38,  79, 181,
+        191, 237, 163, 103, 139, 110, 247,  72, 196,  74, 144,
+        191, 209,   0, 141, 185, 211,  76, 205,  84, 162, 102,
+        38,   6, 215, 169, 216, 176, 130, 234, 205
+      ]
+    )
+  )
+
+  const signMessage = (message) => {
+      const messageBytes = new TextEncoder().encode(message);
+      const signature = nacl.sign.detached(messageBytes, keypair.secretKey);
+
+      return bs58.encode(signature)
+  }
 
   const flowFn = async (node, topic, msg) => {
     switch (msg.state) {
       case "Proof":
         // armar el tx con msg.text[...]
-        await sendMsg(node, msg.replyTo, topic, 'Sent', `TX sent: jhkjhkjhkjhkj`)
-        // Simulate confirmation...
-        await sleep(2000)
-        await sendMsg(node, msg.replyTo, topic, 'Error', `TX la comiste`)
+        // await sendMsg(node, msg.replyTo, topic, 'Sent', `TX sent: jhkjhkjhkjhkj`)
+        // // Simulate confirmation...
+        // await sleep(2000)
+        // await sendMsg(node, msg.replyTo, topic, 'Error', `TX la comiste`)
 
+        try {
+          const pubKey = keypair.publicKey.toBase58()
+          const text = "TX sent: jhkjhkjhkjhkj"
+          const signedMsg = signMessage(text)
+
+          await sendMsg({
+            node,
+            topic: msg.replyTo,
+            replyTo: topic,
+            state: 'Cypher',
+            pubKey,
+            signedMsg,
+            msg: text
+          })
+        } catch (e) {
+          console.error('Error sending message:', e);
+        }
         break;
       default:
         console.log("[flowFn] unknown state: ", msg.state)
@@ -27,6 +65,8 @@ const main = async () => {
   }
 
   await subscribeTo(node, HANDSHAKE_TOPIC, async (node, topic, msg) => {
+    console.log("Subscribed to HANDSHAKE")
+    try {
     switch (msg.state) {
       case "Handshake":
         // Create a random topic to receive messages
@@ -34,12 +74,22 @@ const main = async () => {
 
         await subscribeTo(node, privTopic, flowFn);
 
-        await sendMsg(node, msg.replyTo, privTopic, 'ACK', "whatever")
+        await sendMsg({
+          node,
+          topic: msg.replyTo,
+          replyTo: privTopic,
+          state: 'ACK',
+          msg: "whatever"
+        })
         break;
       default:
         console.log("[handshake] unknown state: ", msg.state)
         break;
     }
+    } catch (e) {
+      console.error('Error subscribe message:', e);
+    }
+
   });
 };
 
